4389175a202dc97a6fac1949c715f378aed1849a
thomasasfk <wee.tommy99@gmail.com>
2025-02-19 23:58:16 +0000

    add nav html webpage and update setup with server

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
 sudo apt update
-sudo apt install -y git docker.io docker-compose
+sudo apt install -y git docker.io docker-compose tmux
+wget -qO- https://astral.sh/uv/install.sh | sh
 sudo usermod -aG docker $USER
 
 if [ ! -f .env ]; then
     echo "TZ=UTC" > .env
 fi
 
-cd docker && docker-compose up -d
+. ~/.bashrc
+
+tmux new-session -d -s httpserver
+tmux send-keys -t httpserver 'cd www' C-m
+tmux send-keys -t httpserver 'uv run python -m http.server 7999' C-m
+
+(cd docker && docker-compose up -d)
+
+tmux new-session -s dev
